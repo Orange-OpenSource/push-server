71573db4c1c95906aaa87bafd842329eeac35c93
Maxime Lardenois <maxime.lardenois@orange.com>
2013-12-17 16:30:07 +0100

    added readme and jslinted

---
+++ README.md
@@ -0,0 +1,53 @@
+PushServer a minimal push server bridging together [Redis](https://github.com/antirez/redis) pub/sub capability and [SockJS](https://github.com/sockjs/sockjs-node).
+
+## Quick start
+
+To install TwitterStreamClient, run: 
+
+```
+npm install orange-pushserver --registry http://10.194.224.187:4873/
+```
+
+Setting up a push server connected to your local Redis instance is as simple as:
+
+```javascript
+var http = require('http'),
+    pushServer = require('orange-pushserver'),
+    server = http.createServer();
+
+pushServer.start(server);
+server.listen(5000);
+```
+
+To send message to SockJS connected clients you only need to publish it on your local Redis `pushserver` channel.
+
+```javascript
+// connect to local redis instance
+var pushServerRedis = redis.createClient();
+
+// send a message
+pushServerRedis.publish('pushserver', 'A cool message for connected browser');
+```
+
+On the browser side, just use [SockJS](https://github.com/sockjs/sockjs-client) to connect to your new PushServer:
+
+```javascript
+var socket = new SockJS('http://localhost:5000/pushserver');
+
+socket.onmessage = function (message) {
+    console.log(message);
+};
+```
+
+You can configure PushServer by passing an options hash to connect():
+
+```javascript
+pushServer.start(server, {
+    channel: 'my-fancy-channel',
+    redisUrl: 'redis://user:password@localhost:6379',
+    prefix: '/fancyurlpath'
+});
+```
+
+## Licence
+MIT
--- index.js
+++ index.js
@@ -1,35 +1,33 @@
+'use strict';
 var sockjs  = require('sockjs'),
-	redis   = require('redis'),
+    redis   = require('redis'),
     url     = require('url');
 
-function start (httpServer, options) {
+function start(httpServer, options) {
     var config = {
             channel: (options && options.channel) || 'pushserver',
             redisUrl: (options && options.redisUrl) || 'redis://localhost:6379',
             prefix: (options && options.prefix) || '/pushserver'
         },
-        password,
         parsedUrl  = url.parse(config.redisUrl),
-        parsedAuth = (parsedUrl.auth || '').split(':');
+        parsedAuth = (parsedUrl.auth || '').split(':'),
+        sockJSServer = sockjs.createServer();
 
-    // Sockjs server
-    var sockJSServer = sockjs.createServer();
-
-    sockJSServer.on('connection', function(sockJSClientConnection) {
+    sockJSServer.on('connection', function (sockJSClientConnection) {
         var pushEventListener = redis.createClient(parsedUrl.port, parsedUrl.hostname);
-        if (password = parsedAuth[1]) {
-            pushEventListener.auth(password, function(err) {
-              if (err) throw err;
+        if (parsedAuth[1]) {
+            pushEventListener.auth(parsedAuth[1], function (err) {
+                if (err) throw err;
             });
         }
 
         pushEventListener.subscribe(config.channel);
 
         // When we see a message on channel, send it to the browser
-        pushEventListener.on('message', function(channel, message) {
+        pushEventListener.on('message', function (channel, message) {
             sockJSClientConnection.write(message);
         });
-        sockJSClientConnection.on('close', function(channel, message) {
+        sockJSClientConnection.on('close', function (channel, message) {
             pushEventListener.end();
         });
     });
