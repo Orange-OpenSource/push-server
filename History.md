8472486e52660c6cc5dc088029418dc3ddcc7603
Maxime Lardenois <maxime.lardenois@orange.com>
2014-06-25 13:09:41 +0200

    error handling too many connection to redis

---
+++ History.md
@@ -0,0 +1,3 @@
+0.4.0 / 2014-06-24
+==================
+* error control when too many simultaneous connections 
--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-'use strict';
-var sockjs  = require('sockjs'),
-    redis   = require('redis'),
-    url     = require('url');
+/*jshint node: true */
+"use strict";
+
+var sockjs = require('sockjs'),
+    redis = require('redis'),
+    url = require('url');
 
 function start(httpServer, options) {
     var config = {
-            channel: (options && options.channel) || 'pushserver',
-            redisUrl: (options && options.redisUrl) || 'redis://localhost:6379',
-            prefix: (options && options.prefix) || '/pushserver'
-        },
-        parsedUrl  = url.parse(config.redisUrl),
+        channel: (options && options.channel) ||  'pushserver',
+        redisUrl: (options && options.redisUrl) ||  'redis://localhost:6379',
+        prefix: (options && options.prefix) ||  '/pushserver',
+        heartbeat_delay: 10,
+        disconnect_delay: 3
+    },
+        parsedUrl = url.parse(config.redisUrl),
         parsedAuth = (parsedUrl.auth || '').split(':'),
         sockJSServer = sockjs.createServer();
 
-    sockJSServer.on('connection', function (sockJSClientConnection) {
+    sockJSServer.on('connection', function(sockJSClientConnection) {
         var pushEventListener = redis.createClient(parsedUrl.port, parsedUrl.hostname);
         if (parsedAuth[1]) {
-            pushEventListener.auth(parsedAuth[1], function (err) {
+            pushEventListener.auth(parsedAuth[1], function(err) {
                 if (err) throw err;
             });
         }
@@ -24,15 +28,22 @@ function start(httpServer, options) {
         pushEventListener.subscribe(config.channel);
 
         // When we see a message on channel, send it to the browser
-        pushEventListener.on('message', function (channel, message) {
+        pushEventListener.on('message', function(channel, message) {
             sockJSClientConnection.write(message);
         });
-        sockJSClientConnection.on('close', function (channel, message) {
-            pushEventListener.end();
+        pushEventListener.on('error', function(err) {
+            console.log('Redis error ' + err);
+            sockJSClientConnection.close();
+        });
+        sockJSClientConnection.on('close', function() {
+            console.log('disconnection from redis on event');
+            pushEventListener.quit();
         });
     });
 
-    sockJSServer.installHandlers(httpServer, {prefix: config.prefix});
+    sockJSServer.installHandlers(httpServer, {
+        prefix: config.prefix
+    });
 }
 
 module.exports = {
